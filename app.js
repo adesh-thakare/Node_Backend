2d10bf13cb83377413e296eb97a164118bd307df
Adesh Thakare <adeshthakare@Adeshs-MacBook-Air.local>
2024-04-14 15:41:50 -0700

    railway postgres connect

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use(cors());
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    url: process.env.DATABASE_URL,
   });
 
 
